f49bf3a6ce46058f08edeed3fa15aabc48c4c8b6
PoriaEbrahimi <poria.ebrahimy@gmail.com>
2019-11-10 05:48:23 -0600

    Pushing a load of merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { useState } from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
 import './App.css';
 import ObjectPage from './ObjectPage/ObjectPage';
 import Navbar from './HeaderComponent/HeaderComponent';
@@ -7,6 +12,7 @@ import HomePage from './HomePage/HomePage';
 import LoginPage from './LoginPage/LoginPage';
 import CartPage from './CartPage/CartPage';
 import OriginPage from './OriginPage/OriginPage';
+
 
 function App() {
 
@@ -54,31 +60,35 @@ function App() {
         ["https://dotesports-media.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2019/10/18142643/senna.jpg", 10]
     ]
     return (
-      <div className="App" style={backgroundStyle}>
-        <Navbar />
-        <OriginPage/>
-        {/*<CartPage*/}
-        {/*    items = {list_items}*/}
-        {/*/>*/}
-        {/*<LoginPage />*/}
-        {/*    logo="https://www.ebuyonline.co.uk/img/ebuy-online-ltd-logo-1525558004.jpg"*/}
-        {/*/>*/}
-        {/*<HomePage*/}
-        {/*    logo="https://www.ebuyonline.co.uk/img/ebuy-online-ltd-logo-1525558004.jpg"*/}
-        {/*    cart="https://icons-for-free.com/iconfiles/png/512/cart-131964784999299812.png"*/}
-        {/*    login="https://www.trzcacak.rs/myfile/detail/3-39618_login-icon-with-transparent-background.png"*/}
-        {/*    img="https://c402277.ssl.cf1.rackcdn.com/photos/14785/images/story_full_width/shutterstock_532108075.jpg?1512507049"*/}
-        {/*    items = {list_items}/>*/}
-        {/*<ObjectPage*/}
-        {/*    img='https://c402277.ssl.cf1.rackcdn.com/photos/14785/images/story_full_width/shutterstock_532108075.jpg?1512507049'*/}
-        {/*    currentPriceOption={price.priceOption}*/}
-        {/*    changePriceOption={priceOptionHandler}*/}
-        {/*    price={price.price}*/}
-        {/*    quantity={price.quantity}*/}
-        {/*    changeQuantity={quantityHandler}/>*/}
-        {/*<Footer />*/}
-    </div>
+          <div className="App" style={backgroundStyle}>
+              <Navbar />
+              <Router>
+                  <Switch>
+                        <Route exact path="/" component={OriginPage}/>
+                        <Route path="/CartPage" component={CartPage}
+                            items = {list_items}
+                        />
+                        <Route path="/LoginPage" component={LoginPage}
+                            logo="https://www.ebuyonline.co.uk/img/ebuy-online-ltd-logo-1525558004.jpg"
+                        />
+                        <Route path="/HomePage" component={HomePage}
+                            logo="https://www.ebuyonline.co.uk/img/ebuy-online-ltd-logo-1525558004.jpg"
+                            cart="https://icons-for-free.com/iconfiles/png/512/cart-131964784999299812.png"
+                            login="https://www.trzcacak.rs/myfile/detail/3-39618_login-icon-with-transparent-background.png"
+                            img="https://c402277.ssl.cf1.rackcdn.com/photos/14785/images/story_full_width/shutterstock_532108075.jpg?1512507049"
+                            items = {list_items}/>
+                        <Route path="/ObjectPage" component={ObjectPage}
+                            img='https://c402277.ssl.cf1.rackcdn.com/photos/14785/images/story_full_width/shutterstock_532108075.jpg?1512507049'
+                            currentPriceOption={price.priceOption}
+                            changePriceOption={priceOptionHandler}
+                            price={price.price}
+                            quantity={price.quantity}
+                            changeQuantity={quantityHandler}/>
+                  </Switch>
+              </Router>
+              <Footer />
+        </div>
   );
 }
-
 export default App;
+
